fc6ef1339c9ade612b5ccb1e09c942833c1d34c5
mashukak <mashenkast12@gmail.com>
2024-10-13 14:36:38 +0200

    task 1 done

--- js/task-1.js
+++ js/task-1.js
@@ -11,3 +11,13 @@ const user = {
   hobby: 'tennis',
   premium: true,
 }
+
+user.mood = 'happy' ;
+
+user.hobby = 'skydiving';
+
+user.premium = false;
+
+for (const key of Object.keys(user)) {
+  console.log(`${key}: ${user[key]}`);
+}
