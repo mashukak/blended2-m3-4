b81292ecd31fd3d72a4546f318fc01421098dcfb
LiliiaOsinska <lo.lihcka@gmail.com>
2024-10-13 16:03:28 +0200

    task5

--- js/task-5.js
+++ js/task-5.js
@@ -1,2 +1,23 @@
 // todo Напишіть функцію updateObject, яка приймає об'єкт і повертає новий об'єкт без вказаних параметрів
 // Результат, що очікується ({a: 1, b: 2, c: 3}, 'b', 'a') => {c: 3}
+// function updateObject(obj, ...rest) {
+//  const newObj = {};
+//  const keys = Object.keys(obj);
+//  for( const key of keys) {
+//     if(!rest.includes(key)){
+//         newObj[key] = obj[key];
+//     }
+//  }
+//  return newObj;
+// }
+
+ function updateObject(obj, ...rest) {
+     const newObj = {...obj};
+     for( const key of rest) {
+    delete newObj[key]
+     }
+     return newObj;
+    }
+
+
+console.log(updateObject({a: 1, b: 2, c: 3},'b', 'a'));
